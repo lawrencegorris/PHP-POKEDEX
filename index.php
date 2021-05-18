0a9c84eead3e6a9501449e48362df31d7c76042c
Lawrence Gorris <gorrislawrence.personal@gmail.com>
2021-05-18 10:30:14 +0200

    Updated UI to a more minimal style

--- index.php
+++ index.php
@@ -13,6 +13,29 @@
 
 <section id="search-box" class="section">
 <form action="">
+    <?php
+    $pokemon_name = '';
+    $pokemon_id = '';
+    $pokemon_image = '';
+    $pokemon_evolutions = '';
+    $pokemon_type = '';
+
+    if(!empty($_GET["pokemon"])){
+        $pokeapi_url = 'https://pokeapi.co/api/v2/pokemon/' . $_GET["pokemon"];
+        $pokespecies_url = 'https://pokeapi.co/api/v2/pokemon-species/' . $_GET["pokemon"];
+
+        $pokemonData = file_get_contents($pokeapi_url);
+        $pokespeciesData = file_get_contents($pokespecies_url);
+
+        $pokemonStats = json_decode($pokemonData, true);
+        $pokespecies = json_decode($pokespeciesData, true);
+
+        $pokemon_name = $pokemonStats['name'];
+        $pokemon_id = $pokemonStats['id'];
+        $pokemon_image = $pokemonStats['sprites']['front_default'];
+        $pokemon_evolutions = $pokespecies;
+    }
+    ?>
     <label>
         Search for a pokemon!
         <br>
@@ -24,46 +47,31 @@
 </section>
 
 <section id="pokemon-stats" class="section">
-    <?php
-        $pokemon_name = '';
-        $pokemon_id = '';
-        $pokemon_image = '';
-        $pokemon_evolutions = '';
 
-        if(!empty($_GET["pokemon"])){
-            $pokeapi_url = 'https://pokeapi.co/api/v2/pokemon/' . $_GET["pokemon"];
-            $pokespecies_url = 'https://pokeapi.co/api/v2/pokemon-species/' . $_GET["pokemon"];
+    <h2 id="pokemon-name"><?php echo ucwords($pokemon_name) ?></h2>
+    <h3 id="pokemon-id"><?php echo '#' . $pokemon_id ?></h3>
+    <ul id="types">
+        <li><?php echo ucwords($pokemonStats['types'][0]['type']['name']) . ' ' ?></li>
+        <li><?php echo ' ' . ucwords($pokemonStats['types'][1]['type']['name']) ?></li>
+    </ul>
 
-            $pokemonData = file_get_contents($pokeapi_url);
-            $pokespeciesData = file_get_contents($pokespecies_url);
-
-            $pokemonStats = json_decode($pokemonData, true);
-            $pokespecies = json_decode($pokespeciesData, true);
-
-            $pokemon_name = $pokemonStats['name'];
-            $pokemon_id = $pokemonStats['id'];
-            $pokemon_image = $pokemonStats['sprites']['front_default'];
-            $pokemon_evolutions = $pokespecies;
-        }
-    ?>
-    <h2><?php echo $pokemon_name ?></h2>
-    <h2><?php echo $pokemon_id ?></h2>
-    <h2>
+    <img src='<?php echo $pokemon_image ?>' alt="Image of currently searched pokemon">
+    <h3>
         <?php
             if($pokemon_evolutions['evolves_from_species'] === null){
                 echo 'This pokemon has no previous form';
             }else{
-                echo 'The previous form of this pokemon is ' . $pokemon_evolutions['evolves_from_species']['name'];
+                echo 'The evolved form of ' . $pokemon_evolutions['evolves_from_species']['name'];
             }?>
-    </h2>
-    <ul>
-        <li><?php echo $pokemonStats['moves'][0]['move']['name'] ?></li>
-        <li><?php echo $pokemonStats['moves'][1]['move']['name'] ?></li>
-        <li><?php echo $pokemonStats['moves'][2]['move']['name'] ?></li>
-        <li><?php echo $pokemonStats['moves'][3]['move']['name'] ?></li>
+    </h3>
+    <ul id="moves">
+        Moves:
+        <li><?php echo ucwords($pokemonStats['moves'][0]['move']['name']) ?></li>
+        <li><?php echo ucwords($pokemonStats['moves'][1]['move']['name']) ?></li>
+        <li><?php echo ucwords($pokemonStats['moves'][2]['move']['name']) ?></li>
+        <li><?php echo ucwords($pokemonStats['moves'][3]['move']['name']) ?></li>
     </ul>
-    <img src='<?php echo $pokemon_image ?>' width=200 alt="Image of currently searched pokemon">
-</section>
 
+</section>
 </body>
 </html>
